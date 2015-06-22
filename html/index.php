e60e9150af059539f632dea80d122302f4362917
crazyyy <crazyyy@gmail.com>
2015-06-22 15:21:16 +0300

    before relise

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <meta name="viewport" content="width=1000px, initial-scale=1"> -->
     <title>Уничтожитель комаров SkeeterVac с бесплатной доставкой по Новосибирску</title>
     <link href="http://www.google-analytics.com/" rel="dns-prefetch"><!-- dns prefetch -->
     <!-- icons -->
